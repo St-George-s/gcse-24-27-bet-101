8d85e00c97d3f7e089d167e255877bbefa3e3b8b
bet-101 <rou005@Stge.org.uk>
2025-10-06 20:56:32 +0000

    Homework

--- logic and arithmetics/logicQuestions.py
+++ logic and arithmetics/logicQuestions.py
@@ -1,14 +1,35 @@
-#Q1
-temperature = int(input("whats the tempature?  "))
-sun = str(input("is it sunny (Y/N):  "))
+# #Q1
+# temperature = int(input("whats the temperature?  "))
+# sun = str(input("is it sunny (Y/N):  "))
 
-if temperature > 15 and sun == "Y":
-    print("YAYAYAYAYAYAYYAYAYAYYAYAY")
-elif temperature > 15:
-    print("wowee")
-elif sun == "N":
-    print("awweeee")
-elif sun == "Y":
-    print("pretty")
-elif temperature <= 15:
-    print("brrr")
+# if temperature > 15 and sun == "Y":
+#     print("YAYAYAYAYAYAYYAYAYAYYAYAY")
+# elif temperature < 15 and sun == "N":
+#     print("big loss")
+# elif temperature > 15:
+#     print("good for you! go get burned")
+# if sun == "N":
+#     print("awweeee")
+# elif sun == "Y":
+#     print("pretty")
+# if temperature <= 15:
+#     print("brrr")
+
+#Q2
+# temperature = int(input("what is the temperature?  "))
+# rain = input("is it raining?  ")
+# if temperature > 15 or rain == "yes":
+#     print("hot or wet")
+
+#Q3
+# apples = int(input("how many apples?  "))
+# if apples <= 10:
+#     print("hungry dawwg")
+
+#Q4 
+age = int(input("how many years, bro?  "))
+licensed = input("do you have a license? (Y/N)?  ")
+if age > 18 and licensed == "Y":
+    print("you can drive fren")
+
+
