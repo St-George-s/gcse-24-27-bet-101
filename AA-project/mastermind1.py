5bc0a34c27788ab582e4159d4c9add6c0d6f3507
bet-101 <rou005@Stge.org.uk>
2026-02-03 10:07:26 +0000

    mastermind

--- AA-project/mastermind1.py
+++ AA-project/mastermind1.py
@@ -15,49 +15,33 @@ elif mode == "easy":
         print("THAT IS NOT RIGHT. 4 NUMBERS!!!!!!!!")
         playerGuess = str(input("what is your guess?(4 NUMBERS)   "))
     while playerGuess != mastermind:
-                for str(index) in range(len(mastermind)):
-                    for str(indexTwo) in range(len(playerGuess)):
-                            if letter in indexTwo == letter in index:
-                                print("this is correct and this is the correct posistion")
-                            elif index in playerGuess[index] != index in mastermind[index]:
-                                print("this is correct but not the correct posistion")
-                            else:
-                                print("this is wrong")
-                            for index in range(len(playerGuess)):
-                                print(index)
-                        counter = counter + 1
-                        print("try again")
-                        print("you have tried", counter, "time(s), not too good")
-                        playerGuess = input("would you like to guess again?(numbers again) ")
-                        while len(playerGuess) != 4:
-                            print("THAT IS NOT RIGHT. 4 NUMBERS!!!!!!!!")
-                            playerGuess = str(input("what is your guess?(4 NUMBERS)   "))
+        for index in range(len(mastermind)):
+            for indexTwo in range(len(playerGuess)):
+                # If the letters match
+                if mastermind[index] == playerGuess[indexTwo]:
+                    #  Letters match
+                    if index == indexTwo:
+                        # Positions match
+                        print("this is correct and in the right posistion")
+                    elif index != indexTwo:
+                         # Positions don't match
+                         print("this is correct but the wrong posistion")
+                elif str(index) in mastermind:  
+                    # If the letter in is mastermind at all
+                    print("this is correct but wrong posistion")
+                else:
+                     print("this is incorrect")
+        counter = counter + 1
+        print("try again")
+        print("you have tried", counter, "time(s), not too good")
+        playerGuess = input("would you like to guess again?(numbers again) ")
+        while len(playerGuess) != 4:
+            print("THAT IS NOT RIGHT. 4 NUMBERS!!!!!!!!")
+            playerGuess = str(input("what is your guess?(4 NUMBERS)   "))
     print("congrats!! you pass")
 else:
     print("that is not a mode. please select again")
     mode = str(input("what mode do you want to play?(hard/easy)   "))
-
-while playerGuess != mastermind:
-    for letter in playerGuess:
-        print(letter)
-        if letter == letter in mastermind:
-            print("this is correct")
-        elif playerGuess == mastermind:
-            print("you are fully correct!")
-        else:
-            print("this is wrong")
-    counter = counter + 1
-    print("try again")
-    print("you have tried", counter, "time(s), not too good")
-    playerGuess = input("would you like to guess again?(numbers again) ")
-    while len(playerGuess) != 4:
-        print("THAT IS NOT RIGHT. 4 NUMBERS!!!!!!!!")
-        playerGuess = str(input("what is your guess?(4 NUMBERS)   "))
-print("congrats!! you pass")
- 
-     
-
-
 
 # Extensions: 
 # 1. Let the user pick an easy mode which shows the user which position that they guessed correctly
