4dd654c0f10777fdca4f35179d352131c5645507
bet-101 <rou005@Stge.org.uk>
2026-03-12 11:31:57 +0000

    linear search

---
+++ schoolwork L5/Searching/new linear.py
@@ -0,0 +1,24 @@
+# linear search
+# create an array
+
+names = ["debbie", "jessie", "vigdis", "emilia"]
+searchValue = "emilia"
+found = False
+index = 0
+
+# conditional loop that stops when
+# 1. - search value is found
+# 2. - the search is at the end of the array
+
+while not found and index < len(names):
+    if searchValue == names[index]:
+        found == True
+    else:
+        index += 1
+
+# after the loop (unindent)
+
+if found:
+    print("found")
+else:
+    print("not found")
